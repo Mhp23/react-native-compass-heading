af0c0f38f9f0edb9169596683ad9d68013536972
M.H.Pousti <52624436+Mhp23@users.noreply.github.com>
2022-05-19 23:09:21 +0430

    updating sensor delay

--- android/src/main/java/com/reactlibrary/compassheading/CompassHeadingModule.java
+++ android/src/main/java/com/reactlibrary/compassheading/CompassHeadingModule.java
@@ -68,8 +68,8 @@ public class CompassHeadingModule extends ReactContextBaseJavaModule implements 
             Sensor gsensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             Sensor msensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
-            sensorManager.registerListener(this, gsensor, SensorManager.SENSOR_DELAY_GAME);
-            sensorManager.registerListener(this, msensor, SensorManager.SENSOR_DELAY_GAME);
+            sensorManager.registerListener(this, gsensor, SensorManager.SENSOR_DELAY_FASTEST);
+            sensorManager.registerListener(this, msensor, SensorManager.SENSOR_DELAY_FASTEST);
 
             mFilter = filter;
             promise.resolve(true);
